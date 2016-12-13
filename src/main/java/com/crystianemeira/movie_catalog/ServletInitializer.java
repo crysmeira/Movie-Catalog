d993149ad5cb99e1a42e01a59b12b97d4cd95a00
Crystiane Meira <crys.0790@gmail.com>
2016-12-12 18:19:45 -0800

    Starting project

---
+++ src/main/java/com/crystianemeira/movie_catalog/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.crystianemeira.movie_catalog;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MovieCatalogApplication.class);
+	}
+
+}
